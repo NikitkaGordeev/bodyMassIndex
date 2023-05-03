8ea8c4357f74986c9f94d3c75f1316490a9c6a71
Nikita G <jobnikitka152rus@gmail.com>
2023-05-03 17:46:12 +0300

    add BmiService

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        int weight = 98;
+        double height = 1.87;
+        double bmi = service.calculate(height, weight);
+        int roundingBmi = (int) bmi;
+        System.out.println("Индекс массы тела: " + roundingBmi);
     }
 }
